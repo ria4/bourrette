b403967c79053ba058683fea4f05104590ef0b66
ria4 <16877076+ria4@users.noreply.github.com>
2021-04-24 11:46:39 +0200

    Further work on peignage

--- peignage.py
+++ peignage.py
@@ -3,15 +3,34 @@ import random
 from gimpfu import *
 
 
-def new_fiber_mask():
-    return mask
+def new_fiber_channel(img, fparams):
+    # Set background and foreground colors
+    gimp.set_background("black")
+    gimp.set_foreground("white")
+
+    # Instantiate black layer
+    flayer = gimp.Layer(img, "Fibers", img.width, img.height,
+                        RGBA_IMAGE, 100, LAYER_MODE_NORMAL)
+    img.add_layer(flayer, 0)
+    pdb.gimp_drawable_edit_fill(flayer, FILL_BACKGROUND)
+
+    # Create channel based on brightness levels
+    # White noise means full effect, black noise means no effect
+    pdb.gimp_selection_all(img)
+    pdb.plug_in_colortoalpha(img, flayer, "black")
+    pdb.gimp_image_select_item(img, CHANNEL_OP_REPLACE, flayer)
+    img.remove_layer(flayer)
+    fchannel = pdb.gimp_selection_save(img)
+
+    return fchannel
+
 
 def peignage(img,
              drawable,
              fibers_width=15,
-             fibers_hardness=50,
-             fibers_density=0,
-             fibers_length=0,
+             fibers_hardness=5,
+             fibers_density=5,
+             fibers_length=5,
              fibers_orientation=0,
              fibers_variation=0,
              overlap_gain=0):
@@ -27,11 +46,17 @@ def peignage(img,
 
     # Store current background color
     bg_color_tmp = gimp.get_background()
+    fg_color_tmp = gimp.get_foreground()
 
-    for layer in img.layers():
-        fiber_mask = new_fiber_mask(img, fparams)
-        # then apply mask
+    # Apply a different fiber mask on every layer
+    for layer in img.layers:
+        fiber_channel = new_fiber_channel(img, fparams)
+        fiber_channel.name = layer.name
+        pdb.gimp_image_set_active_channel(img, fiber_channel)
+        fiber_mask = pdb.gimp_layer_create_mask(layer, ADD_MASK_CHANNEL)
+        pdb.gimp_layer_add_mask(layer, fiber_mask)
 
+    # Add brightness on intersecting regions
     if overlap_gain > 0:
         for layer in img.layers()[1:]:
             overlap = layer.copy()
@@ -40,29 +65,19 @@ def peignage(img,
             pdb.gimp_drawable_brightness_contrast(overlap, overlap_gain / 20.0, 0)
             pdb.gimp_image_merge_down(img, overlap, CLIP_TO_IMAGE)
 
+    # Merge all fiber layers
     pdb.gimp_image_merge_visible_layers(img, CLIP_TO_IMAGE)
 
     # Clean up
     pdb.gimp_selection_none(img)
     gimp.set_background(bg_color_tmp)
+    gimp.set_foreground(fg_color_tmp)
 
     #pdb.gimp_image_undo_group_end(img)
 
 
 
-    # Register layer positon
-    drawable_position = pdb.gimp_image_get_item_position(img, drawable)
-
-    # Create noise layer
-    layer_noise = gimp.Layer(img, "Noise %s" % drawable.name,
-                             img.width, img.height,
-                             RGBA_IMAGE, 100, LAYER_MODE_NORMAL)
-    img.add_layer(layer_noise, drawable_position)
-    if noise_seed == 0:
-        noise_seed = random.getrandbits(32)
-    pdb.plug_in_solid_noise(img, layer_noise, False, False,
-                            noise_seed, noise_detail, noise_size, noise_size)
-
+    """
     # Extract islands
     pdb.gimp_context_set_antialias(False)
     pdb.gimp_context_set_feather(noise_smoothness)
@@ -70,59 +85,28 @@ def peignage(img,
         pdb.gimp_context_set_feather_radius(50, 50)
     pdb.gimp_context_set_sample_threshold(0.5)
     pdb.gimp_image_select_color(img, CHANNEL_OP_REPLACE, layer_noise, "white")
-    gimp.set_background("white")
-    pdb.gimp_drawable_edit_fill(layer_noise, FILL_BACKGROUND)
 
-    # Apply noise threshold
-    threshold = int(noise_threshold * 255 / 100)
     pdb.gimp_selection_invert(img)
-    gimp.set_background(threshold, threshold, threshold)
-    pdb.gimp_drawable_edit_fill(layer_noise, FILL_BACKGROUND)
-
-    # Create channel based on noise brightness levels
-    # White noise means full effect, black noise means no effect
-    pdb.gimp_selection_all(img)
-    pdb.plug_in_colortoalpha(img, layer_noise, "black")
-    pdb.gimp_image_select_item(img, CHANNEL_OP_REPLACE, layer_noise)
-    channel_noise = pdb.gimp_selection_save(img)
-    channel_noise.name = layer_noise.name
-
-    # Add channel mask to drawable duplicate
-    layer_dup = drawable.copy()
-    layer_dup.name = "Duplicate %s" % drawable.name
-    pdb.gimp_image_set_active_channel(img, channel_noise)
-    mask_noise = pdb.gimp_layer_create_mask(layer_dup, ADD_MASK_CHANNEL)
-    pdb.gimp_layer_add_mask(layer_dup, mask_noise)
-
-    # Apply hue-chroma mask
-    img.add_layer(layer_dup, drawable_position + 1)
-    gegl_parameters = "hue-chroma hue=%s chroma=%s" % (hue_shift, chroma_shift)
-    pdb.gegl_gegl(layer_dup, gegl_parameters)
-
-    # Clean up
-    img.remove_layer(layer_noise)
-    pdb.gimp_image_merge_down(img, layer_dup, CLIP_TO_IMAGE)
-    pdb.gimp_selection_none(img)
-    gimp.set_background(bg_color_tmp)
+    """
 
 
 register(
  "python-fu-peignage",
- "Mingle layers like silk fibers"
- "Mingle layers like silk fibers"
+ "Mingle layers like silk fibers",
+ "Mingle layers like silk fibers",
  "Oriane Tury",
  "GNU GPLv3",
  "2021",
  "<Image>/Filters/Peignage",
  "RGB*",
  [
-  (PF_INT32, "noise_seed", "Noise Seed", 0),
-  (PF_SPINNER, "noise_detail", "Noise Detail", 1, (0, 5, 1)),
-  (PF_SLIDER, "noise_size", "Noise Size", 6, (1, 10, 1)),
-  (PF_BOOL, "noise_smoothness", "Noise Smoothness", False),
-  (PF_SLIDER, "noise_threshold", "Noise Threshold", 30, (0, 100, 1)),
-  (PF_SLIDER, "hue_shift", "Hue Adjustment", 0, (-180, 180, 1)),
-  (PF_SLIDER, "chroma_shift", "Chroma Adjustment", 60, (-180, 180, 1)),
+  (PF_SLIDER, "fibers_width", "Fibers Width", 15, (1, 1000, 1)),
+  (PF_SLIDER, "fibers_hardness", "Fibers Hardness", 5, (0, 10, 1)),
+  (PF_SLIDER, "fibers_density", "Fibers Density", 5, (1, 10, 1)),
+  (PF_SLIDER, "fibers_length", "Fibers Length", 5, (1, 10, 1)),
+  (PF_SLIDER, "fibers_orientation", "Fibers Orientation", 0, (-90, 90, 1)),
+  (PF_SLIDER, "fibers_variation", "Fibers Variation", 0, (0, 10, 1)),
+  (PF_SLIDER, "overlap_gain", "Overlap Gain", 0, (0, 10, 1)),
  ],
  [],
  peignage)
